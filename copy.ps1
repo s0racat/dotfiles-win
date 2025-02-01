6f7bc341e2e125bea2e1921378c8e4f9d65dbb73
Takumi <takuoh@tuta.io>
2025-02-01 21:52:24 +0900

    Upd

--- copy.ps1
+++ copy.ps1
@@ -1,8 +1,25 @@
-copy-Item -Verbose .\keepassxc\keepassxc.ini $env:appdata\KeePassXC\keepassxc.ini
-copy-Item -Verbose .\windows-terminal\settings.json $env:localappdata\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState\settings.json
-copy-Item -Verbose .\fancywm\settings.json $env:localappdata\Packages\2203VeselinKaraganev.FancyWM_9x2ndwrcmyd2c\LocalCache\Roaming\FancyWM\settings.json
-copy-Item -Verbose .\wezterm\wezterm.lua $env:USERPROFILE\.wezterm.lua
-copy-Item -Verbose .\firefox\profiles.ini $env:appdata\Mozilla\Firefox\profiles.ini
-copy-Item -Verbose -force -recurse .\firefox\default $env:appdata\Mozilla\Firefox\Profiles
+function Copy-ItemWithDirs {
+    param (
+        [string]$Source,
+        [string]$Destination,
+        [switch]$Recurse
+    )
 
+    $DestDir = Split-Path -Parent $Destination
+    if (!(Test-Path $DestDir)) {
+        New-Item -ItemType Directory -Path $DestDir -Force | Out-Null
+    }
+    
+    if ($Recurse) {
+        Copy-Item -Verbose -Recurse $Source -Destination $Destination
+    } else {
+        Copy-Item -Verbose $Source -Destination $Destination
+    }
+}
 
+Copy-ItemWithDirs .\keepassxc\keepassxc.ini $env:appdata\KeePassXC\keepassxc.ini
+Copy-ItemWithDirs .\windows-terminal\settings.json $env:localappdata\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState\settings.json
+Copy-ItemWithDirs .\fancywm\settings.json $env:localappdata\Packages\2203VeselinKaraganev.FancyWM_9x2ndwrcmyd2c\LocalCache\Roaming\FancyWM\settings.json
+Copy-ItemWithDirs .\wezterm\wezterm.lua $env:USERPROFILE\.wezterm.lua
+Copy-ItemWithDirs .\firefox\profiles.ini $env:appdata\Mozilla\Firefox\profiles.ini
+Copy-ItemWithDirs -Recurse .\firefox\default $env:appdata\Mozilla\Firefox\Profiles
