9b99e931916c5436fde189de28d60299bcd823da
soracat <125882337+s0racat@users.noreply.github.com>
2025-10-30 15:55:30 +0900

    upd

--- powershell/Microsoft.PowerShell_profile.ps1
+++ powershell/Microsoft.PowerShell_profile.ps1
@@ -1,2 +1,44 @@
 Import-Module PSReadline
 Set-PSReadlineOption -EditMode Emacs
+$script:_FirstPrompt = $true
+
+function Prompt {
+    if ($script:_FirstPrompt) {
+        $script:_FirstPrompt = $false
+    } else {
+        Write-Host ""
+    }
+    Write-Host ([char]27 + "[1;38;2;136;192;208m" + "$PWD" + [char]27 + "[0m")
+    return 'PS> '
+}
+
+
+("gl", "gc","gp","rm","curl") | ForEach-Object {
+    $alias = Get-Alias $_ -ErrorAction SilentlyContinue
+    if ($alias) {
+        Remove-Item ("Alias:" + $_) -Force
+    }
+}
+
+function less {
+    & "$env:userprofile\scoop\apps\git\current\usr\bin\less.exe" -R @Args
+}
+function ga { git add $args }
+function gc { git commit $args }
+function gd { git diff $args }
+function gl { git pull $args }
+function gp { git push $args }
+function gs { git status $args }
+
+function rm {
+    param(
+        [string[]]$Path,
+        [switch]$r
+    )
+
+    if ($r) {
+        Remove-Item $Path -Recurse -Force
+    } else {
+        Remove-Item $Path
+    }
+}
