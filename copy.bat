1a49439726c0656c008e1def8ea484ba342ef540
Takumi <125882337+s0racat@users.noreply.github.com>
2026-05-16 20:07:30 +0900

    Update copy.bat

--- copy.bat
+++ copy.bat
@@ -1,20 +1,42 @@
-echo F | xcopy /i /e /y .\keepassxc\keepassxc.ini %appdata%\KeePassXC\keepassxc.ini
-echo F | xcopy /i /e /y .\windows-terminal\settings.json %localappdata%\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState\settings.json
-echo F | xcopy /i /e /y .\fancywm\settings.json %localappdata%\Packages\2203VeselinKaraganev.FancyWM_9x2ndwrcmyd2c\LocalCache\Roaming\FancyWM\settings.json
-echo F | xcopy /i /e /y .\wezterm\wezterm.lua %USERPROFILE%\.wezterm.lua
-echo F | xcopy /i /e /y .\firefox\profiles.ini %appdata%\Mozilla\Firefox\profiles.ini
-xcopy /i /e /y .\firefox\default\* %appdata%\Mozilla\Firefox\Profiles\default
-xcopy /i /e /y zebar\a %userprofile%\.glzr\zebar\a
-echo F | xcopy /i /e /y glazewm\config.yaml %userprofile%\.glzr\glazewm\config.yaml
-echo  F|xcopy   /i  /e  /y  komorebi\applications.json %userprofile%
-echo  F|xcopy   /i  /e  /y  komorebi\komorebi.json %userprofile%
-echo  F|xcopy   /i  /e  /y  komorebi\komorebi.bar.json %userprofile%
-echo  F|xcopy   /i  /e  /y  komorebi\whkdrc %userprofile%\.config
-echo  F|xcopy   /i  /e  /y  alacritty\alacritty.toml %appdata%\alacritty\alacritty.toml
+@echo off
 
-echo  F|xcopy   /i  /e  /y  powershell\Microsoft.PowerShell_profile.ps1 %userprofile%\Documents\WindowsPowerShell
-echo  F|xcopy   /i  /e  /y  powershell\Microsoft.PowerShell_profile.ps1 %userprofile%\Documents\PowerShell
+mkdir "%appdata%\KeePassXC" 2>nul
+mkdir "%localappdata%\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState" 2>nul
+mkdir "%localappdata%\Packages\2203VeselinKaraganev.FancyWM_9x2ndwrcmyd2c\LocalCache\Roaming\FancyWM" 2>nul
+mkdir "%appdata%\Mozilla\Firefox\Profiles\default" 2>nul
+mkdir "%userprofile%\.glzr\zebar\a" 2>nul
+mkdir "%userprofile%\.glzr\glazewm" 2>nul
+mkdir "%userprofile%\.config" 2>nul
+mkdir "%appdata%\alacritty" 2>nul
+mkdir "%userprofile%\Documents\WindowsPowerShell" 2>nul
+mkdir "%userprofile%\Documents\PowerShell" 2>nul
 
+copy /y ".\keepassxc\keepassxc.ini" "%appdata%\KeePassXC\keepassxc.ini"
 
+copy /y ".\windows-terminal\settings.json" "%localappdata%\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState\settings.json"
+
+copy /y ".\fancywm\settings.json" "%localappdata%\Packages\2203VeselinKaraganev.FancyWM_9x2ndwrcmyd2c\LocalCache\Roaming\FancyWM\settings.json"
+
+copy /y ".\wezterm\wezterm.lua" "%USERPROFILE%\.wezterm.lua"
+
+copy /y ".\firefox\profiles.ini" "%appdata%\Mozilla\Firefox\profiles.ini"
+
+xcopy ".\firefox\default\*" "%appdata%\Mozilla\Firefox\Profiles\default\" /i /e /y
+
+xcopy "zebar\a" "%userprofile%\.glzr\zebar\a\" /i /e /y
+
+copy /y "glazewm\config.yaml" "%userprofile%\.glzr\glazewm\config.yaml"
+
+copy /y "komorebi\applications.json" "%userprofile%\applications.json"
+copy /y "komorebi\komorebi.json" "%userprofile%\komorebi.json"
+copy /y "komorebi\komorebi.bar.json" "%userprofile%\komorebi.bar.json"
+
+copy /y "komorebi\whkdrc" "%userprofile%\.config\whkdrc"
+
+copy /y "alacritty\alacritty.toml" "%appdata%\alacritty\alacritty.toml"
+
+copy /y "powershell\Microsoft.PowerShell_profile.ps1" "%userprofile%\Documents\WindowsPowerShell\Microsoft.PowerShell_profile.ps1"
+
+copy /y "powershell\Microsoft.PowerShell_profile.ps1" "%userprofile%\Documents\PowerShell\Microsoft.PowerShell_profile.ps1"
 
 pause
